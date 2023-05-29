d8de5da997642e658fb1cb8b24a936fd7b91a50e
riyuuichi.hirano <RIYUUICHI.HIRANO@grp.tmnf.jp>
2023-05-29 13:54:44 +0900

    getAllMessagesを追加

--- src/models/Tasklist.java
+++ src/models/Tasklist.java
@@ -7,10 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
+
+
 @Entity
-@Table(name = "tasklist")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Tasklist AS m ORDER BY m.id DESC"
+    )
+})
+
+@Table(name = "tasks")
 
 public class Tasklist {
     @Id
@@ -59,10 +70,6 @@ public class Tasklist {
     public void setContent(String content) {
         this.content = content;
     }
-
-
-
-
 
 
 }
